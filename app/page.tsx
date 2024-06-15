9c1170844e228bdf7b5f84550d3b8a64df28026e
mrmorthius <morten_2104@hotmail.com>
2024-06-15 11:42:46 +0200

    added shadcn and nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default async function Home() {
   return (
     <main>
       <h1>Homepage</h1>
+      <Button variant={"default"}>Click me</Button>
     </main>
   );
 }
